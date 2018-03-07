66c571707322617a8b2699d0495d1f8ebe7b8dca
MrAaronCasanova <1boris231@gmail.com>
2018-03-07 09:56:13 -0800

    creating basic recipe app

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
 import './RecipeApp.css';
+import Navbar from './Navbar';
+
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Recipe
           title="Pasta"
           ingredients={['flour', 'water']}
